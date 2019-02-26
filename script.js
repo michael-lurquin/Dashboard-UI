ca787d0197e9854e671bbc43829851820e732a44
Michaël Lurquin <michael.lurquin@gmail.com>
2019-02-26 18:51:34 +0100

    Update

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById('toc').addEventListener('click', toogleMenu);
 
 function toogleMenu(e) {
     var parent = e.target;
-    if ( e.target.tagName === 'I' ) parent = e.target.parentElement;
-    if ( e.target.tagName === 'a' ) parent = e.target.parentElement.parentElement;
+    if ( e.target.tagName === 'I' ) parent = e.target.parentElement.parentElement;
+    if ( e.target.tagName === 'a' ) parent = e.target.parentElement;
     if ( !parent.classList.contains('open') ) {
         parent.classList.add('open');
         parent.children[0].children[0].classList.remove('fa-angle-double-right');
