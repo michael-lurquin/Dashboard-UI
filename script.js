8362f5cdc4ad5dbd4fb0d66fe61e68322ea44de8
Michaël Lurquin <michael.lurquin@gmail.com>
2019-02-26 17:51:37 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('toc').addEventListener('click', toogleMenu);
+
+function toogleMenu(e) {
+    var parent = null;
+    if ( e.target.tagName === 'I' ) parent = e.target.parentElement;
+    parent = parent.parentElement;
+    if ( !parent.classList.contains('open') ) {
+        parent.classList.add('open');
+        parent.children[0].children[0].classList.remove('fa-angle-double-right');
+        parent.children[0].children[0].classList.add('fa-angle-double-left');
+        document.getElementsByTagName('NAV')[0].classList.add('active');
+    }
+    else {
+        parent.classList.remove('open');
+        parent.children[0].children[0].classList.remove('fa-angle-double-left');
+        parent.children[0].children[0].classList.add('fa-angle-double-right');
+        document.getElementsByTagName('NAV')[0].classList.remove('active');
+    }
+}
